82a971302978c5731dbc2ea42f5a6f5ca7afbe90
Mohit Kumar Patel <85980940+mohit1523@users.noreply.github.com>
2024-07-05 14:10:21 +0530

    add

--- frontend/socket.js
+++ frontend/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io("http://https://madbook-api.vercel.app/:3000", {
+export const socket = io("https://madbook-api.vercel.app/:3000", {
     autoConnect: false,
     secure: true,
     transports: ['websocket', 'polling'],

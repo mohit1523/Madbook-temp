ec2ae7a65921fa9385d034882bce56ec255f1673
Mohit Kumar Patel <85980940+mohit1523@users.noreply.github.com>
2024-07-05 13:24:13 +0530

    updating socket code

--- frontend/socket.js
+++ frontend/socket.js
@@ -2,4 +2,6 @@ import io from "socket.io-client";
 
 export const socket = io("http://https://madbook-api.vercel.app/:3000", {
     autoConnect: false,
+    secure: true,
+    transports: ['websocket', 'polling'],
 });
